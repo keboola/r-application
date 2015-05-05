fdb8b019803fe305964d9ce4c0c8a860553a2700
Odin <odinuv@gmail.com>
2015-05-05 08:36:54 +0200

    initial import

---
+++ R/application.R
@@ -0,0 +1,180 @@
+#' Generic abstract class for an application. Provides basic functions
+#'  like logging, working with environment and error handling
+#' @import methods
+#' @export Application
+#' @exportClass Application
+Application <- setRefClass(
+    'Application',
+    fields = list(
+        debugMode = 'logical',
+        # for wrapTryCatch
+        hasFailed = 'logical',
+        messages = 'list',
+        warnings = 'list'
+    ),
+    methods = list(
+        #' Constructor.
+        #'
+        #' @exportMethod
+        initialize = function(debugMode = FALSE) {
+            debugMode <<- debugMode
+        },
+
+        #' Constructor.
+        #'
+        #` @param debugMode Logical TRUE to turn debugging on.
+        #' @exportMethod
+        setDebugMode = function(debugMode) {
+            debugMode <<- debugMode
+        },
+
+        #' Verify that a value is empty
+        #' @param Character or number or vector of those.
+        empty = function(obj) {
+            if (class(obj) == 'character') {
+                ret <- (nchar(obj) == 0)
+            } else if (class(obj) == 'numeric') {
+                ret <- obj == 0
+            } else if (is.null(obj)) {
+                ret <- TRUE
+            } else if (is.na(obj)) {
+                ret <- TRUE
+            }
+            if (length(ret) > 1) {
+                # it was a vector, check if all values of the vector wer empty
+                if (length(ret[ret == TRUE]) == length(ret)) {
+                    ret <- TRUE
+                } else {
+                    ret <- FALSE
+                }
+            }
+            ret
+        },
+        
+        #' Get system environment variable
+        #' @param name Name of the environment variable
+        #' @return Value of the environment variable or null if it does not exist.
+        #' @export
+        getEnv = function(name) {
+            value <- Sys.getenv(name)
+            if (empty(value)) {
+                ret <- NULL
+            } else {
+                ret <- value
+            }
+            ret
+        },
+                
+        #' Log a debugging message
+        #' @param msg Arbitrary msg or printable object
+        #' @export
+        logDebug = function(obj) {
+            if (debugMode) {
+                printLog(obj, 'stdout')
+            }
+        },
+        
+        #' Log a debugging message
+        #' @param msg Arbitrary msg or printable object
+        #' @export
+        logInfo = function(obj) {
+            printLog(obj, 'stdout')
+        },
+        
+        #' Log a debugging message
+        #' @param msg Arbitrary msg or printable object
+        #' @export
+        logError = function(obj) {
+            printLog(obj, 'stderr')
+        },
+
+        #' Helper function to print timestamp with each message
+        #' @param msg Arbitrary msg or printable object
+        #' @param mode Logging mode either 'stdout' or 'stderr'
+        #' @return void
+        printLog = function(msg, mode = 'stdout') {
+            printOut <- ""
+            con <- textConnection("printOut", open = "w", local = TRUE)
+            sink(con, type = c("output", "message"))
+            if (is.character(msg)) {
+#                printOut <- 
+                    print(paste(format(
+                    Sys.time(), "%Y-%m-%d %H:%M:%OS3"), ':',
+                    msg))
+            } else {
+                #printOut <- 
+                    print(format(
+                    Sys.time(), "%Y-%m-%d %H:%M:%OS3"))
+                #printOut <- 
+                    print(msg)
+            }
+            sink(NULL, type = c("output", "message"))
+            close(con)
+            if (mode == 'stdout') {
+                write(printOut, stdout())
+            } else {
+                write(printOut, stderr())
+            }
+        },
+
+        #' Error handling wrapper which prints Java like stack trace in case of error.
+        #' Comes from http://stackoverflow.com/a/24884348/41640
+        #'
+        wrapTryCatch = function(expr, silentSuccess = FALSE, stopIsFatal = TRUE) {
+            hasFailed <<- FALSE
+            messages <<- list()
+            warnings <<- list()
+            logger <- function(obj) {
+                # Change behaviour based on type of message
+                level = sapply(class(obj), switch, debug="DEBUG", message="INFO", warning="WARN", caughtError = "ERROR",
+                               error=if (stopIsFatal) "FATAL" else "ERROR", "")
+                level = c(level[level != ""], "ERROR")[1]
+                simpleMessage = switch(level, DEBUG=,INFO=TRUE, FALSE)
+                quashable = switch(level, DEBUG=,INFO=,WARN=TRUE, FALSE)
+                
+                # Format message
+                time  = format(Sys.time(), "%Y-%m-%d %H:%M:%OS3")
+                txt   = conditionMessage(obj)
+                if (!simpleMessage) txt = paste(txt, "\n", sep="")
+                msg = paste(time, level, txt, sep=" ")
+                calls = sys.calls()
+                calls = calls[1:length(calls)-1]
+                trace = limitedLabels(c(calls, attr(obj, "calls")))
+                if (!simpleMessage && length(trace) > 0) {
+                    trace = trace[length(trace):1]
+                    msg = paste(msg, "  ", paste("at", trace, collapse="\n  "), "\n", sep="")
+                }
+                
+                # Output message
+                if (silentSuccess && !hasFailed && quashable) {
+                    messages <<- append(messages, msg)
+                    if (level == "WARN") warnings <<- append(warnings, msg)
+                } else {
+                    if (silentSuccess && !hasFailed) {
+                        cat(paste(messages, collapse=""))
+                        hasFailed <<- TRUE
+                    }
+                    cat(msg)
+                }
+                
+                # Muffle any redundant output of the same message
+                optionalRestart = function(r) { res = findRestart(r); if (!is.null(res)) invokeRestart(res) }
+                optionalRestart("muffleMessage")
+                optionalRestart("muffleWarning")
+            }
+            vexpr = withCallingHandlers(
+                withVisible(expr),
+                debug=logger, message=logger, warning=logger, caughtError=logger, error=logger
+            )
+            if (silentSuccess && !hasFailed) {
+                cat(paste(warnings, collapse=""))
+            }
+            if (vexpr$visible) vexpr$value else invisible(vexpr$value)
+        },
+
+        #' Main entry point
+        run = function() {
+            stop("Not implemented.")
+        }
+    )
+)
---
+++ tests/testthat/testApplication.R
@@ -0,0 +1,72 @@
+test_that("empty", {
+    app <- Application$new()
+    
+    expect_equal(app$empty(NULL), TRUE)
+    expect_equal(app$empty(NA), TRUE)
+    expect_equal(app$empty(""), TRUE)
+    expect_equal(app$empty(0), TRUE)
+    expect_equal(app$empty(c()), TRUE)
+    expect_equal(app$empty(c(NA)), TRUE)
+    expect_equal(app$empty(c("", "")), TRUE)
+    
+    expect_equal(app$empty(c("", 1)), FALSE)
+})
+
+test_that("logging error channel", {
+    app <- Application$new()
+    app$setDebugMode(TRUE)    
+    time <- format(Sys.time(), "%Y")
+
+    # error channel
+    con <- textConnection("messages", "w")
+    sink(con, type = "message")
+    app$logDebug("Test debug1 message")
+    app$logInfo("Test info1 message")
+    app$logError("Test error1 message")
+    sink(NULL, type = "message")
+    close(con)
+
+    messages <- paste(messages, collapse = " ")    
+    expect_match(messages, time)
+    expect_that(messages, not(matches('info1')))
+    expect_that(messages, not(matches('debug1')))
+    expect_that(messages, matches('error1'))
+})
+
+test_that("logging info channel", {
+    app <- Application$new()
+    app$setDebugMode(TRUE)
+    time <- format(Sys.time(), "%Y")
+    con <- textConnection("messages", "w")
+    sink(con, type = "output")
+    app$logDebug("Test debug2 message")
+    app$logInfo("Test info2 message")
+    app$logError("Test error2 message")
+    sink(NULL, type = "output")
+    close(con)
+
+    messages <- paste(messages, collapse = " ")
+    expect_match(messages, time)
+    expect_that(messages, matches('info2'))
+    expect_that(messages, matches('debug2'))
+    expect_that(messages, not(matches('error2')))
+})
+
+test_that("logging info channel without debug", {
+    app <- Application$new()
+    app$setDebugMode(FALSE)
+    time <- format(Sys.time(), "%Y")
+    con <- textConnection("messages", "w")
+    sink(con, type = "output")
+    app$logDebug("Test debug3 message")
+    app$logInfo("Test info3 message")
+    app$logError("Test error3 message")
+    sink(NULL, type = "output")
+    close(con)
+
+    messages <- paste(messages, collapse = " ")    
+    expect_match(messages, time)
+    expect_that(messages, matches('info3'))
+    expect_that(messages, not(matches('debug3')))
+    expect_that(messages, not(matches('error3')))
+})
